ff979fdc24286f841f6c467a05f4576370947c30
Khaled M <k.code@icloud.com>
2018-12-09 22:34:41 -0500

    added functionality to input field

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -10,3 +10,15 @@ $('span').click(function (e) {
     });
     e.stopPropagation();
 });
+
+// Adds items to list from input field
+$("input[type='text']").keypress(function (e) { 
+    if(e.which === 13) {
+        //grabs the new todo from input
+        var todoText = $(this).val();
+        //empties input field
+        $(this).val("");
+        //creates a new li and adds todoText to it
+        $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+    }
+});
